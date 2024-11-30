3c5573a62a0d288e410249de5fc25438f97bc566
shahdatef <shahdatef346@gmail.com>
2024-11-30 07:02:18 +0200

    first commit

---
+++ Models/StudentLecture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class StudentLecture
+    {
+        public string StudentId { get; set; }
+        public Student Student { get; set; }
+        public int LectureId { get; set; }
+        public Lecture Lecture { get; set; }
+
+    }
+}
